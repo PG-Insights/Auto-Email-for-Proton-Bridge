5bfec41e79de4d8f222aa004c516a9458ce635ba
quaaalud <dludwins@outlook.com>
2023-02-24 18:27:30 -0600

    simplified ssh login

--- __helpers__/ssh_login.py
+++ __helpers__/ssh_login.py
@@ -7,66 +7,26 @@ Created on Sun Feb 19 09:41:54 2023
 """
 
 import os
-import json
 from fabric import Connection
 from dotenv import load_dotenv
 
 load_dotenv()
 
-class DoubleQuoteJSONEncoder(json.JSONEncoder):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.encoder = json.JSONEncoder(
-            ensure_ascii=False,
-            separators=(',', ':'),
-            sort_keys=True
-        ).encode
 
-    def encode(self, obj):
-        def sanitize(obj):
-            if isinstance(obj, dict):
-                return {'"' + k + '"': sanitize(v) for k, v in obj.items()}
-            elif isinstance(obj, list):
-                return [sanitize(elem) for elem in obj]
-            else:
-                return f'"{obj}"'
-        return self.encoder(sanitize(obj))
+def return_ssh_connection(
+        path_to_ssh=os.environ.get('SSH_PATH'),
+        hostname = os.environ.get('VM_HOST')
+        ) -> Connection:
+    # Login to the remote machine and return connection
+    return Connection(hostname, 
+                      user='opc', 
+                      connect_kwargs={'key_filename': path_to_ssh}
+                      ) 
 
 
-def ssh_login_and_run_function(func_to_run, **kwargs):
-    path_to_ssh = os.environ.get('SSH_PATH')
-    hostname = os.environ.get('VM_HOST')
-    main_dir = r'/home/opc/email_venv'
-    helpers_dir = r'/home/opc/email_venv/__helpers__'
-    # Serialize kwargs as a JSON string
-    kwargs_str = json.dumps(kwargs, 
-                            separators=(',', ':'),
-                            cls=DoubleQuoteJSONEncoder
-                            )
-
-    # Build the Python command to execute the function
-    command = [
-    'python3.11', '-c',
-    f'''"import json; import os, sys; from pathlib import Path; sys.path.append('{main_dir}'); sys.path.append('{helpers_dir}'); from {func_to_run.__module__} import {func_to_run.__name__}; {func_to_run.__name__}(**{kwargs})"'''
-    ]
-    command_str = ' '.join(command)
-    # Login to the remote machine and execute the command
-    with Connection(hostname, 
-                    user='opc', 
-                    connect_kwargs={'key_filename': path_to_ssh}
-                    ) as conn:
-        print(conn)
-        conn.run('cd /home/opc/email_venv && ls -a', hide=True, echo=True)
-        result = conn.run(command_str, hide=True, echo=True)
-
-        if result.failed:
-            print('This is printing because it didnt work')
-            #print(result.stderr)
-        else:
-            print(result.stdout)
-            print('\nThis is printing because it should have worked?')
 
 if __name__ == '__main__':
     def test_func(str_var='This Is A Test'):
         print(str_var + str_var)
-    ssh_login_and_run_function(test_func, html='Hello, world!')
+    conn = return_ssh_connection()
+    print(conn)
